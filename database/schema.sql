e70660165bb59140a2747cfd6bf0e477ef8734a0
Nevyn Brown <108302716+BrownTD@users.noreply.github.com>
2025-03-18 13:22:46 -0400

    Updated schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,158 @@
+-- MySQL dump 10.13  Distrib 8.4.4, for macos15 (arm64)
+--
+-- Host: localhost    Database: fundu_db
+-- ------------------------------------------------------
+-- Server version	8.4.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `campaigns`
+--
+
+DROP TABLE IF EXISTS `campaigns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `campaigns` (
+  `organization_id` int NOT NULL,
+  `org_name` varchar(100) NOT NULL,
+  `campaign_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `description` text NOT NULL,
+  `donation_goal` int NOT NULL,
+  `current_donations` decimal(10,2) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `ended_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`campaign_id`),
+  KEY `fk_campaigns_user_id_idx` (`created_by`),
+  KEY `fk_campaigns_oid_idx` (`organization_id`),
+  CONSTRAINT `fk_campaigns_oid` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`organization_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_campaigns_uid` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `donations`
+--
+
+DROP TABLE IF EXISTS `donations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donations` (
+  `campaign_id` int NOT NULL,
+  `donation_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `donation_date` datetime NOT NULL,
+  PRIMARY KEY (`donation_id`),
+  KEY `fk_donations_cid_idx` (`campaign_id`),
+  KEY `fk_donations_uid_idx` (`user_id`),
+  CONSTRAINT `fk_donations_cid` FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`campaign_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_donations_uid` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organization_members`
+--
+
+DROP TABLE IF EXISTS `organization_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organization_members` (
+  `organization_id` int NOT NULL,
+  `org_member_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `role` enum('member','manager') NOT NULL,
+  `joined_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`org_member_id`),
+  KEY `fk_organization_member_oid_idx` (`organization_id`),
+  KEY `fk_organization_member_uid` (`user_id`),
+  CONSTRAINT `fk_organization_member_oid` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`organization_id`),
+  CONSTRAINT `fk_organization_member_uid` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `organizations`
+--
+
+DROP TABLE IF EXISTS `organizations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organizations` (
+  `organization_id` int NOT NULL AUTO_INCREMENT,
+  `org_name` varchar(100) NOT NULL,
+  `manager_email` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `school` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `manager_uid` int NOT NULL,
+  PRIMARY KEY (`organization_id`),
+  KEY `fk_organization_uid_idx` (`manager_uid`),
+  CONSTRAINT `fk_organization_uid` FOREIGN KEY (`manager_uid`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `donation_id` int NOT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `transaction_date` datetime DEFAULT NULL,
+  `transaction_status` enum('pending','completed','failed') DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `fk_transactions_did_idx` (`donation_id`),
+  CONSTRAINT `fk_transactions_did` FOREIGN KEY (`donation_id`) REFERENCES `donations` (`donation_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` enum('donor','member','manager') NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-18 13:11:09
